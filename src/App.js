b55dfbd6c555300af7429caf70d3c855204c313f
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-05-09 02:20:27 +0900

    local stream

--- src/App.js
+++ src/App.js
@@ -1,22 +1,98 @@
 import "./App.css";
-import ReactDOMServer from "react-dom/server";
-import callModal from "./callModal";
+import React, { useRef, useState } from "react";
+
+const WebcamVideo = () => {
+  const [stream, setStream] = useState(null);
+  const videoRef = useRef(null);
+
+  const startWebcam = async () => {
+    try {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      setStream(mediaStream);
+      if (videoRef.current) {
+        videoRef.current.srcObject = mediaStream;
+      }
+    } catch (error) {
+      console.error("Error accessing webcam:", error);
+    }
+  };
+
+  return (
+    <>
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "20px",
+          color: "white",
+        }}
+      />
+      <button
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#25D366",
+          color: "#FFFFFF",
+          borderRadius: "50%",
+          width: "80px",
+          height: "80px",
+          margin: "10px",
+          border: "none",
+          outline: "none",
+          cursor: "pointer",
+        }}
+        onClick={startWebcam}
+      >
+        Камера
+      </button>
+    </>
+  );
+};
 
 const App = () => {
   const handleClick = () => {
     const newWindow = window.open("", "", "height=600,width=800");
-    const callModalString = ReactDOMServer.renderToString(callModal());
-    newWindow.document.body.innerHTML = `<div class="callModal">${callModalString}</div>`;
-    // Define a function to close the new window
-    function closeWindow() {
-      newWindow.close();
-    }
+    newWindow.document.body.innerHTML = `<div class="callModal" style="
+      background-color: #282c34;
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      font-size: calc(10px + 2vmin);
+      color: white;"></div>`;
+    newWindow.document.documentElement.style.margin = "0";
+    newWindow.document.body.style.margin = "0";
 
-    // Add a button to the callModal that calls the closeWindow function
-    const closeButton = document.createElement("button");
-    closeButton.innerText = "Close Window";
-    closeButton.addEventListener("click", closeWindow);
-    newWindow.document.querySelector(".callModal").appendChild(closeButton);
+    newWindow.document.querySelector(".callModal").innerHTML = `
+      <WebcamVideo />
+      <button
+        style="
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-color: #E45649;
+          color: #FFFFFF;
+          border-radius: 50%;
+          width: 80px;
+          height: 80px;
+          margin: 10px;
+          border: none;
+          outline: none;
+          cursor: pointer;
+        "
+        onclick="window.close()"
+      >
+        Положить трубку
+      </button>
+    `;
   };
 
   return (
