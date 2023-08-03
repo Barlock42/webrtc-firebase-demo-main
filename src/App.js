7d38badf1a2d0aae51b918d6eb9eb19bcd69b372
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-08-03 13:20:49 +0900

    sad

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
     doc,
     collection,
     setDoc,
+    onSnapshot,
     serverTimestamp,
 } from "firebase/firestore";
 
@@ -88,21 +89,22 @@ const App = () => {
         // Use serverTimestamp here
         await setDoc(callDoc, { offer, timestamp });
 
-        // Listen for remote answer
-        callDoc.onSnapshot((snapshot) => {
-            const data = snapshot.data();
-            if (!pc.currentRemoteDescription && data?.answer) {
+        // Subscribe to changes using onSnapshot and Listen for remote answer
+        onSnapshot(callDoc, (docSnapshot) => {
+            // Handle document snapshot changes here
+            const data = docSnapshot.data();
+            if (!pcRef.current.currentRemoteDescription && data?.answer) {
                 const answerDescription = new RTCSessionDescription(data.answer);
-                pc.setRemoteDescription(answerDescription);
+                pcRef.current.setRemoteDescription(answerDescription);
             }
         });
 
         // When answered, add candidate to peer connection
-        answerCandidates.onSnapshot((snapshot) => {
-            snapshot.docChanges().forEach((change) => {
-                if (change.type === 'added') {
+        onSnapshot(answerCandidatesRef, (docSnapshot) => {
+            docSnapshot.docChanges().forEach((change) => {
+                if (change.type === "added") {
                     const candidate = new RTCIceCandidate(change.doc.data());
-                    pc.addIceCandidate(candidate);
+                    pcRef.current.addIceCandidate(candidate);
                 }
             });
         });
@@ -111,12 +113,50 @@ const App = () => {
     const acceptCall = async () => {
         setIsAcceptingCall((prevVisibility) => !prevVisibility);
         setupWebRTC();
+
+        const callId = callInput.value;
+        const callDoc = firestore.collection('calls').doc(callId);
+        const answerCandidates = callDoc.collection('answerCandidates');
+        const offerCandidates = callDoc.collection('offerCandidates');
+
+        pc.onicecandidate = (event) => {
+            event.candidate && answerCandidates.add(event.candidate.toJSON());
+        };
+
+        const callData = (await callDoc.get()).data();
+
+        const offerDescription = callData.offer;
+        await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+        const answerDescription = await pc.createAnswer();
+        await pc.setLocalDescription(answerDescription);
+
+        const answer = {
+            type: answerDescription.type,
+            sdp: answerDescription.sdp,
+        };
+
+        await callDoc.update({ answer });
+
+        offerCandidates.onSnapshot((snapshot) => {
+            snapshot.docChanges().forEach((change) => {
+                console.log(change);
+                if (change.type === 'added') {
+                    let data = change.doc.data();
+                    pc.addIceCandidate(new RTCIceCandidate(data));
+                }
+            });
+        });
     }
 
     const startWebcam = async () => {
     }
 
     const stopWebcam = async () => {
+    }
+
+    const toggleCamVisibility = async () => {
+        
     }
 
     return (
@@ -137,7 +177,7 @@ const App = () => {
                             !isLocalCamVisibility ? "Включить камеру" : "Выключить камеру"
                         }
                         clickHandler={!isLocalCamVisibility ? startWebcam : stopWebcam}
-                        toggleVisibility={() => { }}
+                        toggleVisibility={toggleCamVisibility}
                     ></Button>
                     <Button
                         color={"red"}
