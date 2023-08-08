9077d52807bff9dd2b886af48deac1f63b62f610
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-08-08 22:50:07 +0900

    servers

--- src/App.js
+++ src/App.js
@@ -24,7 +24,14 @@ import {
 const servers = {
     iceServers: [
         {
-            urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+            urls: [
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+                "stun:stun.l.google.com:19302",
+                "stun:stun3.l.google.com:19302",
+                "stun:stun4.l.google.com:19302",
+                "stun:stun.services.mozilla.com",
+              ],
         },
     ],
     iceCandidatePoolSize: 10,
