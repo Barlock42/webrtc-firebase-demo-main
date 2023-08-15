b499924ff450c3025c25bb8506b79d5b325b705d
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-08-15 14:58:43 +0900

    firebase down

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import dbRef, { userName, connctorRef } from "./server/firebase";
+import dbRef, { userName, connectorRef } from "./server/firebase";
 import "./App.css";
 import { useEffect } from "react";
 
-const App = () => { 
-    useEffect()
+const App = () => {
+    const participantRef = dbRef.child("participants");
+
+    // 1.) Component mounts -› effect runs -> creates listener
+    // 2.) Component unmounts -> clean up -> remove listener
+    // 3.) Component mounts -> effect runs -> creates listener 
+    useEffect(() => {
+        const handleValueChange = (snap) => {
+            if (snap.val()) {
+                const defaultPreference = {
+                    audio: true,
+                    video: false,
+                    screen: false,
+                };
+                // console.log("pushing")
+                const userRef = participantRef.push({ userName, preferences: defaultPreference });
+                userRef.onDisconnect().remove();
+            }
+        };
+    
+        connectorRef.on('value', handleValueChange);
+    
+        return () => {
+            // Clean up the event listener and onDisconnect
+            connectorRef.off('value', handleValueChange);
+            participantRef.off('value'); // Clean up other listeners if needed
+        };
+    }, [participantRef]);
+    
+
     return (
         <div className="App">
             <header className="App-header"></header>
-            <div style={{ display: "flex" }}>
-                {userName}
-            </div>
+            <div>{userName}</div>
         </div>);
 }
 
--- src/server/firebase.js
+++ src/server/firebase.js
@@ -1,9 +1,10 @@
 import firebaseConfig from "../firebaseConfig";
-import firebase from "./firebase";
+import firebase from "firebase/app";
+import 'firebase/database';
 
 firebase.initializeApp(firebaseConfig);
 let dbRef = firebase.database().ref();
-export const connctorRef = firebase.database().ref(".info/connected");
+export const connectorRef = firebase.database().ref(".info/connected");
 
 export const userName = "test"; // Get it from outer application
 const urlParams = new URLSearchParams(window.location.search);
