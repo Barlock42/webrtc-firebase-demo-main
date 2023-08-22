5cc4c365fb73ecc6583a0ba5b03bf3a6d07d0b3e
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-08-23 00:36:04 +0900

    refactor

--- src/App.js
+++ src/App.js
@@ -1,80 +1,104 @@
-import dbRef, { userName, connectorRef } from "./server/firebase";
+import MainScreen from "./components/MainScreen/MainScreen";
+import firepadRef, { db, userName } from "./config/firebase";
 import "./App.css";
 import { useEffect } from "react";
-import { connect } from "react-redux"
-import { setUser, addParticipant, removeParticipant } from "./store/actionCreator";
-import { MainScreen } from "./components/MainScreen/MainScreen.js";
+import {
+  setMainStream,
+  addParticipant,
+  setUser,
+  removeParticipant,
+  updateParticipant,
+} from "./store/actioncreator";
+import { connect } from "react-redux";
 
-const App = (props) => {
-    const participantRef = dbRef.child("participants");
+function App(props) {
+  const getUserStream = async () => {
+    const localStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
 
-    // 1.) Component mounts -› effect runs -> creates listener
-    // 2.) Component unmounts -> clean up -> remove listener
-    // 3.) Component mounts -> effect runs -> creates listener 
-    useEffect(() => {
-        const handleValueChange = (snap) => {
-            if (snap.val()) {
-                const defaultPreferences = {
-                    audio: true,
-                    video: false,
-                    screen: false,
-                };
-                // console.log("pushing")
-                const userRef = participantRef.push({ userName, preferences: defaultPreferences });
-                props.setUser({
-                    [userRef.key]: { userName, ...defaultPreferences }
-                });
-                userRef.onDisconnect().remove();
-            }
+    return localStream;
+  };
+  useEffect(async () => {
+    const stream = await getUserStream();
+    stream.getVideoTracks()[0].enabled = false;
+    props.setMainStream(stream);
+
+    connectedRef.on("value", (snap) => {
+      if (snap.val()) {
+        const defaultPreference = {
+          audio: true,
+          video: false,
+          screen: false,
         };
+        const userStatusRef = participantRef.push({
+          userName,
+          preferences: defaultPreference,
+        });
+        props.setUser({
+          [userStatusRef.key]: { name: userName, ...defaultPreference },
+        });
+        userStatusRef.onDisconnect().remove();
+      }
+    });
+  }, []);
 
-        connectorRef.on('value', handleValueChange);
+  const connectedRef = db.database().ref(".info/connected");
+  const participantRef = firepadRef.child("participants");
 
-        return () => {
-            // Clean up the event listener and onDisconnect
-            connectorRef.off('value', handleValueChange);
-            participantRef.off('value'); // Clean up other listeners if needed
-        };
-    }, [participantRef]);
+  const isUserSet = !!props.user;
+  const isStreamSet = !!props.stream;
 
-    useEffect(() => {
-        if (props.user) {
-            participantRef.on("child_added", handleChildAdded)
-            participantRef.on("child_removed", handleChildRemoved);
-        }
+  useEffect(() => {
+    if (isStreamSet && isUserSet) {
+      participantRef.on("child_added", (snap) => {
+        const preferenceUpdateEvent = participantRef
+          .child(snap.key)
+          .child("preferences");
+        preferenceUpdateEvent.on("child_changed", (preferenceSnap) => {
+          props.updateParticipant({
+            [snap.key]: {
+              [preferenceSnap.key]: preferenceSnap.val(),
+            },
+          });
+        });
+        const { userName: name, preferences = {} } = snap.val();
+        props.addParticipant({
+          [snap.key]: {
+            name,
+            ...preferences,
+          },
+        });
+      });
+      participantRef.on("child_removed", (snap) => {
+        props.removeParticipant(snap.key);
+      });
+    }
+  }, [isStreamSet, isUserSet]);
 
-        const handleChildAdded = (snap) => {
-            const { userName, preferences } = snap.val();
-            props.addParticipant({ [snap.key]: { userName, ...preferences } });
-        };
-
-        const handleChildRemoved = (snap) => {
-            props.removeParticipant([snap.key]);
-        };
-    }, [props.user]);
-
-    return (
-        <div className="App">
-            <header className="App-header"></header>
-            <div>
-                <MainScreen/ >
-            </div>
-        </div>);
+  return (
+    <div className="App">
+      <MainScreen />
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
-    return {
-        user: state.currentUser,
-        participants: state.participants
-    }
-}
+  return {
+    stream: state.mainStream,
+    user: state.currentUser,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        setUser: (user) => dispatch(setUser(user)),
-        addParticipant: (participant) => dispatch(addParticipant(participant)),
-        removeParticipant: (participantKey) => dispatch(setUser(participantKey))
-    }
-}
+  return {
+    setMainStream: (stream) => dispatch(setMainStream(stream)),
+    addParticipant: (user) => dispatch(addParticipant(user)),
+    setUser: (user) => dispatch(setUser(user)),
+    removeParticipant: (userId) => dispatch(removeParticipant(userId)),
+    updateParticipant: (user) => dispatch(updateParticipant(user)),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { Provider } from "react-redux";
-import { configureStore } from '@reduxjs/toolkit';
+import { createStore } from "redux";
 import { userReducer } from "./store/reducer";
 
-const rootReducer = {
-  user: userReducer // 'user' is a key to identify this reducer in the store
-};
-
-export const store = configureStore({
-  reducer: rootReducer
-});
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+export const store = createStore(userReducer);
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
